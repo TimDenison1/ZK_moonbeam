47d52e9fe56672b4cb345ff037e24b1ac222c170
Jack0fClubs <44550756+JackofClubz@users.noreply.github.com>
2022-09-04 15:48:48 +0100

    Update zk_primitive.rs

--- zk_primitive.rs
+++ zk_primitive.rs
@@ -113,6 +113,71 @@ fn fixed_scalar_mult(from: &[u8; 32], p_g: &jubjub::SubgroupPoint) -> jubjub::Su
 
     p_g * f
 }
+// Silence some warnings so they don't distract from the exercise.
+#![allow(unused_mut, unused_variables)]
+
+fn main() {
+    // This fancy stuff either gets the first argument as a String, or prints
+    // usage and exits if an argument was not supplied to the program.
+    let mut arg: String = std::env::args().nth(1).unwrap_or_else(|| {
+        println!("Please supply an argument to this program.");
+        std::process::exit(-1);
+    });
+
+    // 1. Write a function `inspect` that takes a reference to a String, returns nothing, but
+    // prints whether the contents of the String is plural or singular. Then uncomment and run this
+    // code with `cargo run apple` and `cargo run apples'.  Hint: use `.ends_with("s")` on the
+    // String reference
+    //
+    inspect(&arg);
+
+    // 2. Write a function `change` that takes a *mutable* reference to a String and adds an "s" to
+    // the String if it doesn't already end with "s". Then uncomment and run the code below with
+    // `cargo run apple`.  Hint: use `.push_str("s")` on the mutable String reference to add an "s".
+    //
+    change(&mut arg);
+    println!("I have many {}", arg);
+
+    // 3. Write a function `eat` that accepts ownership of (consumes) a String and returns a bool
+    // indicating whether or not the String both starts with a "b" AND contains an "a".
+    // Hint 1: use `.starts_with("b")` and `.contains("a")`
+    // Hint 2: `&&` is the boolean "AND" operator
+    //
+    if eat(arg) {
+        println!("Might be bananas");
+    } else {
+        println!("Not bananas");
+    }
+
+    // Try running this program with "boat", "banana", and "grapes" as the arguments :-)
+
+    // Challenge: Write a function "add" that takes *references* to two integer arguments,
+    // dereferences them and adds them together, and returns the result.
+    //
+    println!("1 + 2 = {}, even via references", add(&1, &2));
+
+    fn inspect(s: &String){
+        if s.ends_with("s"){
+            println!("{}, is plural", s);
+        }else{
+            println!("{}, is singular",s);
+        }
+    }
+
+    fn change(s: &mut String) {
+        if !s.ends_with("s") {
+            s.push_str("s");
+        }
+    }
+
+    fn eat(consumes: String) -> bool{
+        consumes.starts_with("b") && consumes.contains("a")
+    }
+
+    fn add(x: &i32, y: &i32) -> i32{
+        *x + *y
+    }
+}
 
 /// Loads the zk-SNARK parameters into memory and saves paths as necessary.
 /// Only called once.
@@ -206,14 +271,7 @@ pub extern "C" fn librustzcash_init_zksnark_params(
 /// Writes the "uncommitted" note value for empty leaves of the Merkle tree.
 ///
 /// `result` must be a valid pointer to 32 bytes which will be written.
-#[no_mangle]
-pub extern "C" fn librustzcash_tree_uncommitted(result: *mut [c_uchar; 32]) {
-    let tmp = Note::uncommitted().to_bytes();
 
-    // Should be okay, caller is responsible for ensuring the pointer
-    // is a valid pointer to 32 bytes that can be mutated.
-    let result = unsafe { &mut *result };
-    *result = tmp;
 }
 
 /// Computes a merkle tree hash for a given depth. The `depth` parameter should
